662a3bccf0aedb84a4cc3830f847671c87d99df9
ernaneaugusto <ernaneatcipriano@gmail.com>
2019-02-17 20:46:07 -0300

    Finalizado layout wire section#about

--- wp-content/themes/educaejoga/includes/sections/about.php
+++ wp-content/themes/educaejoga/includes/sections/about.php
@@ -1,13 +1,28 @@
-<section class="container">
+<section class="container" id="about">
     <div class="row">
-        <div class="col-12">
 
+        <div class="col-12">
             <h2 class="section-title">Sobre</h2>
             <p class="section-description">
                 <span class="description-bold">EMPREENDEDORES E EDUCADORES</span>
                 Lorem ipsum dolor sit amet.
             </p>
-
+        </div>
+        <div class="col-lg-6 col-12">
+            <div class="about-item text-center">
+                <!-- <img src="<?php bloginfo("template_url")?>/img/nomedaimagem.jpg" alt="" title="" /> -->
+                <img src="https://via.placeholder.com/150" alt="" title="" class="about-img" />
+                <h3 class="about-title">Para você Educador</h3>
+                <p class="about-description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non, ut! Numquam cumque neque natus reprehenderit iste? A, architecto cumque. Nobis?</p>
+            </div>
+        </div>
+        <div class="col-lg-6 col-12 mt-4 mt-lg-0">
+            <div class="about-item text-center">
+                <!-- <img src="<?php bloginfo("template_url")?>/img/nomedaimagem.jpg" alt="" title="" /> -->
+                <img src="https://via.placeholder.com/150" alt="" title="" class="about-img" />
+                <h3 class="about-title">Para você Empreendedor</h3>
+                <p class="about-description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non, ut! Numquam cumque neque natus reprehenderit iste? A, architecto cumque. Nobis?</p>
+            </div>
         </div>
     </div>
 </section>
