9b39b410b1636c1890fccc72a20475a70db47295
ernaneaugusto <ernaneatcipriano@gmail.com>
2019-02-17 20:57:22 -0300

    Finalizado layout wire section#participants

--- wp-content/themes/educaejoga/includes/sections/participants.php
+++ wp-content/themes/educaejoga/includes/sections/participants.php
@@ -1,10 +1,40 @@
-<section class="container" id="participants">
-    <div class="row">
-        <div class="col-12">
+<section id="participants">
+    <div class="container">
 
-            <h2 class="section-title">Participantes</h2>
-            <p class="section-description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita labore esse impedit perferendis minus nulla.</p>
-            
+        <div class="row">
+            <div class="col-12">
+                <h2 class="section-title">Participantes</h2>
+                <p class="section-description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita labore esse impedit perferendis minus nulla.</p>                
+            </div>            
+        </div>
+        <div class="row justify-content-center">            
+            <div class="col-lg-4 col-12">
+                <div class="text-center">
+                    <!-- <img src="<?php bloginfo("template_url")?>/img/nomedaimagem.jpg" alt="" title="" /> -->
+                    <img src="https://via.placeholder.com/150" alt="" title="" class="participants-img" />
+                    <h3 class="participants-title">Nome do palestrante</h3>
+                    <h4 class="participants-ocupation">Head de Educação na Empresa xxx</h4>
+                    <p class="participants-description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non, ut! Numquam cumque neque natus reprehenderit iste? A, architecto cumque. Nobis?</p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-12">
+                <div class="text-center">
+                    <!-- <img src="<?php bloginfo("template_url")?>/img/nomedaimagem.jpg" alt="" title="" /> -->
+                    <img src="https://via.placeholder.com/150" alt="" title="" class="participants-img" />
+                    <h3 class="participants-title">Nome do palestrante</h3>
+                    <h4 class="participants-ocupation">Head de Educação na Empresa xxx</h4>
+                    <p class="participants-description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non, ut! Numquam cumque neque natus reprehenderit iste? A, architecto cumque. Nobis?</p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-12">
+                <div class="text-center">
+                    <!-- <img src="<?php bloginfo("template_url")?>/img/nomedaimagem.jpg" alt="" title="" /> -->
+                    <img src="https://via.placeholder.com/150" alt="" title="" class="participants-img" />
+                    <h3 class="participants-title">Nome do palestrante</h3>
+                    <h4 class="participants-ocupation">Head de Educação na Empresa xxx</h4>
+                    <p class="participants-description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non, ut! Numquam cumque neque natus reprehenderit iste? A, architecto cumque. Nobis?</p>
+                </div>
+            </div>
         </div>
     </div>
 </section>
