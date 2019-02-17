c388594ce5bf3cb1c0c3c3725b28591aef320733
ernaneaugusto <ernaneatcipriano@gmail.com>
2019-02-17 20:07:30 -0300

    separados sections em includes no arquivo index.php

---
+++ wp-content/themes/educaejoga/includes/sections/about.php
@@ -0,0 +1,13 @@
+<section class="container">
+    <div class="row">
+        <div class="col-12">
+
+            <h2 class="section-titile">Sobre</h2>
+            <p class="section-description">
+                <span class="description-bold">EMPREENDEDORES E EDUCADORES</span>
+                Lorem ipsum dolor sit amet.
+            </p>
+            
+        </div>
+    </div>
+</section>
--- wp-content/themes/educaejoga/index.php
+++ wp-content/themes/educaejoga/index.php
@@ -1,7 +1,10 @@
 <?php include('includes/layout/header.php'); ?>
 
 
-
+<?php get_template_part("includes/sections/main"); ?>
+<?php get_template_part("includes/sections/about"); ?>
+<?php get_template_part("includes/sections/participants"); ?>
+<?php get_template_part("includes/sections/who-we-are"); ?>
 
 
 <?php include('includes/layout/footer.php'); ?>
