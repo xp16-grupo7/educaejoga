30da8daf972a97a1a07a9fd98e88fca4022b8043
ernaneaugusto <ernaneatcipriano@gmail.com>
2019-02-17 23:53:05 -0300

    Finalizado layout section#about

--- wp-content/themes/educaejoga/includes/sections/about.php
+++ wp-content/themes/educaejoga/includes/sections/about.php
@@ -3,13 +3,10 @@
 
         <div class="col-12">
             <h2 class="section-title">Sobre</h2>
-            <p class="section-description">
-                <span class="description-bold">EMPREENDEDORES E EDUCADORES</span>
-                Lorem ipsum dolor sit amet.
-            </p>
+            <p class="section-description">O GamificaÊ é um evento que une escolas e empresas que buscam inovação e destaque utilizando a gamificação como solução para a educação EAD.</p>
         </div>
         <div class="col-lg-6 col-12">
-            <div class="about-item text-center">
+            <div class="about-item item-purple text-center">
                 <!-- <img src="<?php bloginfo("template_url")?>/img/nomedaimagem.jpg" alt="" title="" /> -->
                 <img src="https://via.placeholder.com/150" alt="" title="" class="about-img" />
                 <h3 class="about-title">Para você Educador</h3>
@@ -17,7 +14,7 @@
             </div>
         </div>
         <div class="col-lg-6 col-12 mt-4 mt-lg-0">
-            <div class="about-item text-center">
+            <div class="about-item item-pink text-center">
                 <!-- <img src="<?php bloginfo("template_url")?>/img/nomedaimagem.jpg" alt="" title="" /> -->
                 <img src="https://via.placeholder.com/150" alt="" title="" class="about-img" />
                 <h3 class="about-title">Para você Empreendedor</h3>
