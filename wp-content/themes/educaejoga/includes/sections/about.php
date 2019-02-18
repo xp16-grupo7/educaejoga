1d83c7166767a8d4e9d2283a26cfb9fde1fbef21
ernaneaugusto <ernaneatcipriano@gmail.com>
2019-02-18 00:36:43 -0300

    Finalizado section#participants

--- wp-content/themes/educaejoga/includes/sections/about.php
+++ wp-content/themes/educaejoga/includes/sections/about.php
@@ -7,18 +7,16 @@
         </div>
         <div class="col-lg-6 col-12">
             <div class="about-item item-purple text-center">
-                <!-- <img src="<?php bloginfo("template_url")?>/img/nomedaimagem.jpg" alt="" title="" /> -->
-                <img src="https://via.placeholder.com/150" alt="" title="" class="about-img" />
-                <h3 class="about-title">Para você Educador</h3>
-                <p class="about-description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non, ut! Numquam cumque neque natus reprehenderit iste? A, architecto cumque. Nobis?</p>
+                <img src="<?php bloginfo("template_url")?>/img/icon1.png" alt="" title="" class="about-img" />       
+                <h3 class="about-title">Para você, Educador</h3>
+                <p class="about-description">Buscamos profissionais que desejam reinventar a educação brasileira. Acreditamos que a gamificação transforma não só o modelo de ensino mas também os resultados obtidos por quem utiliza!</p>
             </div>
         </div>
         <div class="col-lg-6 col-12 mt-4 mt-lg-0">
             <div class="about-item item-pink text-center">
-                <!-- <img src="<?php bloginfo("template_url")?>/img/nomedaimagem.jpg" alt="" title="" /> -->
-                <img src="https://via.placeholder.com/150" alt="" title="" class="about-img" />
-                <h3 class="about-title">Para você Empreendedor</h3>
-                <p class="about-description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non, ut! Numquam cumque neque natus reprehenderit iste? A, architecto cumque. Nobis?</p>
+                <img src="<?php bloginfo("template_url")?>/img/icon2.png" alt="" title="" class="about-img" />
+                <h3 class="about-title">Para você, Gestor</h3>
+                <p class="about-description">Descubra as útimas novidades no mercado da educação corporativa. Entenda como a tecnologia  pode impactar positivamente o seu negócio.</p>
             </div>
         </div>
     </div>
--- wp-content/themes/educaejoga/includes/sections/participants.php
+++ wp-content/themes/educaejoga/includes/sections/participants.php
@@ -3,38 +3,20 @@
 
         <div class="row">
             <div class="col-12">
-                <h2 class="section-title">Participantes</h2>
-                <p class="section-description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita labore esse impedit perferendis minus nulla.</p>                
+                <h2 class="section-title">Participantes do GamificaÊ</h2>
+                <!-- <p class="section-description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita labore esse impedit perferendis minus nulla.</p>                 -->
             </div>            
         </div>
         <div class="row justify-content-center">            
-            <div class="col-lg-4 col-12">
+            <div class="col-lg-4 col-12 participants-item">
                 <div class="text-center">
-                    <!-- <img src="<?php bloginfo("template_url")?>/img/nomedaimagem.jpg" alt="" title="" /> -->
-                    <img src="https://via.placeholder.com/150" alt="" title="" class="participants-img" />
-                    <h3 class="participants-title">Nome do palestrante</h3>
-                    <h4 class="participants-ocupation">Head de Educação na Empresa xxx</h4>
-                    <p class="participants-description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non, ut! Numquam cumque neque natus reprehenderit iste? A, architecto cumque. Nobis?</p>
+                    <img src="<?php bloginfo("template_url")?>/img/palestrante1.png" alt="Fernanda Lima" title="Fernanda Lima" />
+                    <h3 class="participants-title">Fernanda Lima</h3>
+                    <!-- <h4 class="participants-ocupation">Head de Educação na Empresa xxx</h4> -->
+                    <p class="participants-description">É pedagoga, atuou na área de RH desenvolvendo universidades corporativas, é psicanalista e atualmente está estuda coaching para adolescentes</p>
                 </div>
             </div>
-            <div class="col-lg-4 col-12">
-                <div class="text-center">
-                    <!-- <img src="<?php bloginfo("template_url")?>/img/nomedaimagem.jpg" alt="" title="" /> -->
-                    <img src="https://via.placeholder.com/150" alt="" title="" class="participants-img" />
-                    <h3 class="participants-title">Nome do palestrante</h3>
-                    <h4 class="participants-ocupation">Head de Educação na Empresa xxx</h4>
-                    <p class="participants-description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non, ut! Numquam cumque neque natus reprehenderit iste? A, architecto cumque. Nobis?</p>
-                </div>
-            </div>
-            <div class="col-lg-4 col-12">
-                <div class="text-center">
-                    <!-- <img src="<?php bloginfo("template_url")?>/img/nomedaimagem.jpg" alt="" title="" /> -->
-                    <img src="https://via.placeholder.com/150" alt="" title="" class="participants-img" />
-                    <h3 class="participants-title">Nome do palestrante</h3>
-                    <h4 class="participants-ocupation">Head de Educação na Empresa xxx</h4>
-                    <p class="participants-description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non, ut! Numquam cumque neque natus reprehenderit iste? A, architecto cumque. Nobis?</p>
-                </div>
-            </div>
+            
         </div>
     </div>
 </section>
