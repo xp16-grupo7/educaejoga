0ec94e27407abe40abfa787247920a7394e10ee3
Eder Rodrigo Fleming <ederfleming@hotmail.com>
2019-02-18 10:24:37 -0300

    Ajustes seção quem somos e formulário

--- wp-content/themes/educaejoga/includes/sections/who-we-are.php
+++ wp-content/themes/educaejoga/includes/sections/who-we-are.php
@@ -1,9 +1,10 @@
-<section id="who-we-are">
+<section  id="who-we-are">>
     <div class="container">
-        <div class="row">
-            <div class="col-12">
+        <div class="row"  >
+            <div class="col-12 text-center">
 
-                <h2 class="section-title">Quem Somos</h2>
+                <h2 class="section-title" >Quem Somos</h2>
+                <p class="learn-description">Acreditamos que a gamificação transforma não só o modelo de ensino mas também os resultados obtidos por quem utiliza!</p>
                             
             </div>
         </div>
@@ -11,8 +12,7 @@
     <div class="row justify-content-center">            
             <div class="col-lg-4 col-12">
                 <div class="text-center">
-                    <!-- <img src="<?php bloginfo("template_url")?>/img/nomedaimagem.jpg" alt="" title="" /> -->
-                    <img src="https://via.placeholder.com/150" alt="" title="" class="who-we-are-img" />
+                    <img src="<?php bloginfo("template_url")?>\img\icone-elearning.png" alt="" title="" />
                     <h3 class="learn-title">E-learning</h3>
                     <p class="learn-description">Aprendizado contínuo online sem interferir nos compromissos presenciais diários</p>
                 </div>
@@ -21,8 +21,7 @@
             <div class="row justify-content-center">            
             <div class="col-lg-4 col-12">
                 <div class="text-center">
-                    <!-- <img src="<?php bloginfo("template_url")?>/img/nomedaimagem.jpg" alt="" title="" /> -->
-                    <img src="https://via.placeholder.com/150" alt="" title="" class="who-we-are-img" />
+                    <img src="<?php bloginfo("template_url")?>\img\icone-gamificacao.png" alt="" title="" />
                     <h3 class="gamification-title">Gamificação</h3>
                     <p class="gamification-description text-center">Aplicação de tecnologias gamificadas no método de ensino clássico. Elevamos sua transmissão de conteúdo para um novo patamar (é sério)</p>
                 </div>
