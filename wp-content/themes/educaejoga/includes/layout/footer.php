d2c3277bbf550238adc819604d95deebff965a03
ernaneaugusto <ernaneatcipriano@gmail.com>
2019-02-17 21:13:08 -0300

    Finalizado layout wire footer

--- wp-content/themes/educaejoga/includes/layout/footer.php
+++ wp-content/themes/educaejoga/includes/layout/footer.php
@@ -1,3 +1,21 @@
+
+<footer class="footer">
+    <section class="container">
+        <div class="row">
+            <div class="col text-center">
+                <ul class="footer-nav">
+                    <li><a href="#" class="footer-link">Blog</a></li>
+                    <li><a href="#" class="footer-link">Compre</a></li>
+                    <li><a href="#" class="footer-link">Fale conosco <span class="footer-email"></span></a></li>
+                </ul>
+            </div>
+        </div>
+    </section>
+    <section class="footer-copyright">
+        <a href="<?= site_url("/"); ?>" class="copyright-link">Educa e Joga <?php echo date("Y")?></a>
+    </section>
+</footer>
+
 <script src="<?php bloginfo("template_url"); ?>/dist/app.js"></script>
 <?php wp_footer(); ?>
 </body>
