697c92c43c1adf80103a93c35de754a7c5d5140e
ernaneaugusto <ernaneatcipriano@gmail.com>
2019-02-17 23:37:27 -0300

    Finalizado layout section#main

--- wp-content/themes/educaejoga/includes/sections/main.php
+++ wp-content/themes/educaejoga/includes/sections/main.php
@@ -3,10 +3,10 @@
         <ul class="swiper-wrapper">
             <li class="swiper-slide carrossel-main-item" style="background-image: url('https://via.placeholder.com/1200x700')">
                 <div class="main-wrapper text-center">
-                    <h1 class="main-title">Nome do evento</h1>
-                    <h2 class="main-subtitle">Subtitulo do evento vai aqui</h2>
-                    <p class="main-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus quia maxime exercitationem quibusdam neque blanditiis.</p>
-                    <a href="" class="btn btn-primary">Compre seu ingresso</a>
+                    <h1 class="main-title">GamificaÊ</h1>
+                    <h2 class="main-subtitle">Evolua. Engaje. Eduque.</h2>
+                    <p class="main-description">O evento que alavanca o sistema de educação de base e corporativo</p>
+                    <a href="" class="btn btn-white">Garanta seu ingresso</a>
                 </div>
             </li>
         </ul>
