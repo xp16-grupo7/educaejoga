778edb1a75b0cf9f2ab2f6f9ccf5053cd6c9911d
Eder Rodrigo Fleming <ederfleming@hotmail.com>
2019-02-18 00:20:24 -0300

    Added who-we-are and call-footer wire

--- wp-content/themes/educaejoga/index.php
+++ wp-content/themes/educaejoga/index.php
@@ -5,6 +5,6 @@
 <?php get_template_part("includes/sections/about"); ?>
 <?php get_template_part("includes/sections/participants"); ?>
 <?php get_template_part("includes/sections/who-we-are"); ?>
-
+<?php get_template_part("includes/sections/call-footer"); ?>
 
 <?php include('includes/layout/footer.php'); ?>
