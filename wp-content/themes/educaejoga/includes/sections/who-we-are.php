3366b108c49ddc8e380674480b296b25c283d82a
ernaneaugusto <ernaneatcipriano@gmail.com>
2019-02-19 00:20:03 -0300

    Ajustado section#who-we-are

--- wp-content/themes/educaejoga/includes/sections/who-we-are.php
+++ wp-content/themes/educaejoga/includes/sections/who-we-are.php
@@ -1,29 +1,30 @@
-<section  id="who-we-are">>
+<section  id="who-we-are">
     <div class="container">
-        <div class="row"  >
+        <div class="row">
             <div class="col-12 text-center">
-
-                <h2 class="section-title" >Quem Somos</h2>
-                <p class="learn-description">Acreditamos que a gamificação transforma não só o modelo de ensino mas também os resultados obtidos por quem utiliza!</p>
-                            
+                <h2 class="section-title">Quem Somos</h2>
+                <p class="section-description">Acreditamos que a gamificação transforma não só o modelo de ensino mas também os resultados obtidos por quem utiliza!</p>
             </div>
         </div>
-    </div>
-    <div class="row justify-content-center">            
-            <div class="col-lg-4 col-12">
-                <div class="text-center">
-                    <img src="<?php bloginfo("template_url")?>\img\icone-elearning.png" alt="" title="" />
-                    <h3 class="learn-title">E-learning</h3>
-                    <p class="learn-description">Aprendizado contínuo online sem interferir nos compromissos presenciais diários</p>
+        <div class="row justify-content-center">
+            <div class="col-lg-5 col-12 who-we-are-items">
+               <div class="who-we-are-icon">
+                    <img src="<?php bloginfo("template_url")?>\img\icone-elearning.png" alt="Ícone E-learning" title="E-learning" />
+               </div>
+                <div class="who-we-are-description">
+                    <h3 class="icon-title">E-learning</h3>
+                    <p class="icon-description">Aprendizado contínuo online sem interferir nos compromissos presenciais diários</p>
                 </div>
             </div>
-    
-            <div class="row justify-content-center">            
-            <div class="col-lg-4 col-12">
-                <div class="text-center">
-                    <img src="<?php bloginfo("template_url")?>\img\icone-gamificacao.png" alt="" title="" />
-                    <h3 class="gamification-title">Gamificação</h3>
-                    <p class="gamification-description text-center">Aplicação de tecnologias gamificadas no método de ensino clássico. Elevamos sua transmissão de conteúdo para um novo patamar (é sério)</p>
+            <div class="col-lg-5 col-12 who-we-are-items">
+               <div class="who-we-are-icon">
+                    <img src="<?php bloginfo("template_url")?>\img\icone-gamificacao.png" alt="Ícone Gamificação" title="Gamificação" />
+               </div>
+                <div class="who-we-are-description">
+                    <h3 class="icon-title">Gamificação</h3>
+                    <p class="icon-description">Aplicação de tecnologias gamificadas no método de ensino clássico. Elevamos sua transmissão de conteúdo para um novo patamar (é sério)</p>
                 </div>
             </div>
+        </div>
+    </div>                  
 </section>
