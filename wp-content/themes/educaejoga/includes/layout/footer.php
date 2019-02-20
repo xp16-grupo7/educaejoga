c7e2b2f7983ae5ccba6db5ba9ed525911aefe352
Eder Rodrigo Fleming <ederfleming@hotmail.com>
2019-02-20 13:32:05 -0300

    Ajustes links do footer

--- wp-content/themes/educaejoga/includes/layout/footer.php
+++ wp-content/themes/educaejoga/includes/layout/footer.php
@@ -1,36 +1,72 @@
-
-<footer class="footer">
-    <section class="container">
-        <div class="row">
-            <div class="col-lg-3 col-12">
-                <ul class="footer-nav">
-                    <li><a href="#" class="footer-link">Sobre o evento</a></li>
-                    <li><a href="#" class="footer-link">Palestrantes</a></li>
-                    <li><a href="#" class="footer-link">Quem somos <span class="footer-email"></span></a></li>
-                </ul>
-            </div>
-            <div class="col-lg-3 col-12">
-                <ul class="footer-nav">
-                    <li><a href="#" class="footer-link">Blog</a></li>
-                    <li><a href="#" class="footer-link">Compra de ingressos</a></li>
-                    <li><a href="#" class="footer-link">Fale conosco <span class="footer-email">contato@email.com</span></a></li>
-                </ul>
-            </div>
-            <div class="col-lg-6 col-12 footer-social-links">
-                <ul class="footer-nav">
-                    <li><a href="https://www.facebook.com/educaejoga/" target="_blank" class="footer-link" title="Acesse nosso Facebook">Facebook <i class="fab fa-facebook-square"></i></a></li>
-                    <li><a href="https://www.instagram.com/educaejoga/" target="_blank" class="footer-link" title="Acesse nosso Instagram">Instagram <i class="fab fa-instagram"></i></a></li>                    
-                    <li><a href="#" target="_blank" class="footer-link" title="Acesse nosso Linkedin">Linkedin <i class="fab fa-linkedin"></i></a></li>                    
-                </ul>
-            </div>
-        </div>
-    </section>
-    <section class="footer-copyright">
-        <a href="<?= site_url("/"); ?>" class="copyright-link">Educa e Joga <?php echo date("Y")?></a>
-    </section>
-</footer>
-
-<script src="<?php bloginfo("template_url"); ?>/dist/app.js"></script>
-<?php wp_footer(); ?>
-</body>
-</html> 
+
+
+<footer class="footer">
+
+    <section class="container">
+
+        <div class="row">
+
+            <div class="col-lg-3 col-12">
+
+                <ul class="footer-nav">
+
+                    <li><a href="#about" class="footer-link">Sobre o evento</a></li>
+
+                    <li><a href="#participants" class="footer-link">Palestrantes</a></li>
+
+                    <li><a href="#who-we-are" class="footer-link">Quem somos <span class="footer-email"></span></a></li>
+
+                </ul>
+
+            </div>
+
+            <div class="col-lg-3 col-12">
+
+                <ul class="footer-nav">
+
+                    <li><a href="http://www.ensinarcomjogos.com.br" class="footer-link">Blog</a></li>
+
+                    <li><a href="#call" class="footer-link">Compra de ingressos</a></li>
+
+                    <li><a href="#" class="footer-link">Fale conosco <span class="footer-email">contato@email.com</span></a></li>
+
+                </ul>
+
+            </div>
+
+            <div class="col-lg-6 col-12 footer-social-links">
+
+                <ul class="footer-nav">
+
+                    <li><a href="https://www.facebook.com/educaejoga/" target="_blank" class="footer-link" title="Acesse nosso Facebook">Facebook <i class="fab fa-facebook-square"></i></a></li>
+
+                    <li><a href="https://www.instagram.com/educaejoga/" target="_blank" class="footer-link" title="Acesse nosso Instagram">Instagram <i class="fab fa-instagram"></i></a></li>                    
+
+                    <li><a href="https://www.linkedin.com/company/educa-joga/" target="_blank" class="footer-link" title="Acesse nosso Linkedin">Linkedin <i class="fab fa-linkedin"></i></a></li>                    
+
+                </ul>
+
+            </div>
+
+        </div>
+
+    </section>
+
+    <section class="footer-copyright">
+
+        <a href="<?= site_url("/"); ?>" class="copyright-link">Educa e Joga <?php echo date("Y")?></a>
+
+    </section>
+
+</footer>
+
+
+
+<script src="<?php bloginfo("template_url"); ?>/dist/app.js"></script>
+
+<?php wp_footer(); ?>
+
+</body>
+
+</html> 
+
